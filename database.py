b4f93d51342dd950ee8859eec57ca1508b61f168
Emiliano Rebolledo <rebolledonavarreteemiliano@gmail.com>
2026-01-28 12:12:09 -0600

    Agregado de los reportes del 2 al 5 tarda el 3

--- database.py
+++ database.py
@@ -55,6 +55,88 @@ class Database:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
     
+    def managers(self):
+        query = """
+        SELECT
+            d.dept_name AS departamento,
+            CONCAT(e.first_name, ' ', e.last_name) AS manager,
+            dm.from_date AS fecha_inicio
+        FROM dept_manager dm
+        JOIN departments d
+            ON dm.dept_no = d.dept_no
+        JOIN employees e
+            ON dm.emp_no = e.emp_no
+        WHERE dm.to_date = '9999-01-01'
+        ORDER BY d.dept_name
+        """
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+
+    def mejores_pagados(self):
+        query = """
+        SELECT
+            d.dept_name AS departamento,
+            e.emp_no AS no_empleado,
+            CONCAT(e.first_name, ' ', e.last_name) AS empleado,
+            s.salary AS salario
+        FROM departments d
+        JOIN dept_emp de
+            ON d.dept_no = de.dept_no
+        AND de.to_date = '9999-01-01'
+        JOIN employees e
+            ON de.emp_no = e.emp_no
+        JOIN salaries s
+            ON e.emp_no = s.emp_no
+        AND s.to_date = '9999-01-01'
+        JOIN (
+            SELECT de2.dept_no, MAX(s2.salary) AS max_salary
+            FROM dept_emp de2
+            JOIN salaries s2
+                ON de2.emp_no = s2.emp_no
+            AND s2.to_date = '9999-01-01'
+            WHERE de2.to_date = '9999-01-01'
+            GROUP BY de2.dept_no
+        ) ms
+            ON ms.dept_no = d.dept_no
+        AND ms.max_salary = s.salary
+        """
+
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def empleados_por_anio(self):
+        query = """
+        SELECT
+            YEAR(hire_date) AS anio,
+            COUNT(*) AS total_empleados
+        FROM employees
+        GROUP BY YEAR(hire_date)
+        ORDER BY anio
+        """
+
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def descripcion_dep(self):
+        query = """
+        SELECT
+            d.dept_name AS departamento,
+            COUNT(de.emp_no) AS total_empleados,
+            ROUND(AVG(s.salary),2) AS salario_prom
+        FROM departments d
+            JOIN dept_emp de
+                ON d.dept_no = de.dept_no
+                AND de.to_date = '9999-01-01'
+            JOIN salaries s
+                ON s.emp_no = de.emp_no
+                AND s.to_date = '9999-01-01'
+        GROUP BY d.dept_no, d.dept_name
+        """
+
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
 if __name__ == "__main__":
     db = Database()
     datos = db.empleados(dept_no='d001')
--- interfaz.py
+++ interfaz.py
@@ -65,20 +65,36 @@ class App(ctk.CTk):
         btn_lista.pack(pady=5)
 
         ctk.CTkLabel(self.contenido_dinamico, text="2.- Managers por departamento").pack(pady=2)
-        btn_managers = ctk.CTkButton(self.contenido_dinamico, text="Reporte 2", command=lambda: print("Reporte 2"))
+        btn_managers = ctk.CTkButton(
+            self.contenido_dinamico, 
+            text = "Reporte 2", 
+            command = self.reporte_2
+            )
         btn_managers.pack(pady=5)
 
         ctk.CTkLabel(self.contenido_dinamico, text="3.- Empleado mejor pagado por departamento").pack(pady=2)
-        btn_mjrPagado = ctk.CTkButton(self.contenido_dinamico, text="Reporte 3", command=lambda: print("Reporte 3"))
+        btn_mjrPagado = ctk.CTkButton(
+            self.contenido_dinamico, 
+            text = "Reporte 3", 
+            command = self.reporte_3
+            )
         btn_mjrPagado.pack(pady=5)
 
         ctk.CTkLabel(self.contenido_dinamico, text="4.- Listado total de empleados por año").pack(pady=2)
-        btn_contratados = ctk.CTkButton(self.contenido_dinamico, text="Reporte 4", command=lambda: print("Reporte 4"))
+        btn_contratados = ctk.CTkButton(
+            self.contenido_dinamico, 
+            text = "Reporte 4", 
+            command = self.reporte_4
+            )
         btn_contratados.pack(pady=5)
 
         ctk.CTkLabel(self.contenido_dinamico, text="5.- Listado descripción de departamento").pack(pady=2)
-        btn_managers = ctk.CTkButton(self.contenido_dinamico, text="Reporte 5", command=lambda: print("Reporte 5"))
-        btn_managers.pack(pady=5)
+        btn_descripcion = ctk.CTkButton(
+            self.contenido_dinamico, 
+            text="Reporte 5", 
+            command=self.reporte_5
+            )
+        btn_descripcion.pack(pady=5)
         
     def reporte_1(self):
         self.limpiar_panel()
@@ -178,6 +194,198 @@ class App(ctk.CTk):
                 emp["departamento"],
                 emp["titulo_actual"],
                 f"${emp['salario_actual']}"
+            ]
+
+            for col, valor in enumerate(valores):
+                ctk.CTkLabel(tabla, text=str(valor)).grid(
+                    row=fila,
+                    column=col,
+                    padx=8,
+                    pady=4,
+                    sticky="w"
+                )
+
+    def reporte_2(self):
+        self.limpiar_panel()
+        self.titulo_vista.configure(text="Listado de managers")
+
+        # --- CONTENEDOR DE TABLA ---
+        self.frame_tabla = ctk.CTkFrame(self.contenido_dinamico)
+        self.frame_tabla.pack(fill="both", expand=True, padx=10, pady=10)
+
+        db = Database()
+        managers = db.managers()
+        db.cerrar()
+
+        # Frame con scroll
+        tabla = ctk.CTkScrollableFrame(self.frame_tabla)
+        tabla.pack(fill="both", expand=True)
+
+        # Encabezados
+        encabezados = [
+            "Departamento",
+            "Manager",
+            "Fecha Inicio"
+        ]
+
+        for col, texto in enumerate(encabezados):
+            lbl = ctk.CTkLabel(
+                tabla,
+                text=texto,
+                font=ctk.CTkFont(weight="bold")
+            )
+            lbl.grid(row=0, column=col, padx=8, pady=6, sticky="w")
+
+        # Filas
+        for fila, man in enumerate(managers, start=1):
+            valores = [
+                man["departamento"],
+                man["manager"],
+                man["fecha_inicio"]
+            ]
+
+            for col, valor in enumerate(valores):
+                ctk.CTkLabel(tabla, text=str(valor)).grid(
+                    row=fila,
+                    column=col,
+                    padx=8,
+                    pady=4,
+                    sticky="w"
+                )
+
+    def reporte_3(self):
+        self.limpiar_panel()
+        self.titulo_vista.configure(text="Mejores pagados por departamento")
+
+        # --- CONTENEDOR DE TABLA ---
+        self.frame_tabla = ctk.CTkFrame(self.contenido_dinamico)
+        self.frame_tabla.pack(fill="both", expand=True, padx=10, pady=10)
+
+        db = Database()
+        mejores_pagados = db.mejores_pagados()
+        db.cerrar()
+
+        # Frame con scroll
+        tabla = ctk.CTkScrollableFrame(self.frame_tabla)
+        tabla.pack(fill="both", expand=True)
+
+        # Encabezados
+        encabezados = [
+            "Departamento",
+            "No. Empleado",
+            "Empleado",
+            "Salario"
+        ]
+
+        for col, texto in enumerate(encabezados):
+            lbl = ctk.CTkLabel(
+                tabla,
+                text=texto,
+                font=ctk.CTkFont(weight="bold")
+            )
+            lbl.grid(row=0, column=col, padx=8, pady=6, sticky="w")
+
+        # Filas
+        for fila, mp in enumerate(mejores_pagados, start=1):
+            valores = [
+                mp["departamento"],
+                mp["no_empleado"],
+                mp["empleado"],
+                f"${mp['salario']}"
+            ]
+
+            for col, valor in enumerate(valores):
+                ctk.CTkLabel(tabla, text=str(valor)).grid(
+                    row=fila,
+                    column=col,
+                    padx=8,
+                    pady=4,
+                    sticky="w"
+                )
+
+    def reporte_4(self):
+        self.limpiar_panel()
+        self.titulo_vista.configure(text="Empleados por año")
+
+        # --- CONTENEDOR DE TABLA ---
+        self.frame_tabla = ctk.CTkFrame(self.contenido_dinamico)
+        self.frame_tabla.pack(fill="both", expand=True, padx=10, pady=10)
+
+        db = Database()
+        empa = db.empleados_por_anio()
+        db.cerrar()
+
+        # Frame con scroll
+        tabla = ctk.CTkScrollableFrame(self.frame_tabla)
+        tabla.pack(fill="both", expand=True)
+
+        # Encabezados
+        encabezados = [
+            "Año",
+            "Total Empleados Contratados"
+        ]
+
+        for col, texto in enumerate(encabezados):
+            lbl = ctk.CTkLabel(
+                tabla,
+                text=texto,
+                font=ctk.CTkFont(weight="bold")
+            )
+            lbl.grid(row=0, column=col, padx=8, pady=6, sticky="w")
+
+        # Filas
+        for fila, data in enumerate(empa, start=1):
+            valores = [
+                data["anio"],
+                data["total_empleados"]
+            ]
+
+            for col, valor in enumerate(valores):
+                ctk.CTkLabel(tabla, text=str(valor)).grid(
+                    row=fila,
+                    column=col,
+                    padx=8,
+                    pady=4,
+                    sticky="w"
+                )
+
+    def reporte_5(self):
+        self.limpiar_panel()
+        self.titulo_vista.configure(text="Descripción de departamentos")
+
+        # --- CONTENEDOR DE TABLA ---
+        self.frame_tabla = ctk.CTkFrame(self.contenido_dinamico)
+        self.frame_tabla.pack(fill="both", expand=True, padx=10, pady=10)
+
+        db = Database()
+        desc = db.descripcion_dep()
+        db.cerrar()
+
+        # Frame con scroll
+        tabla = ctk.CTkScrollableFrame(self.frame_tabla)
+        tabla.pack(fill="both", expand=True)
+
+        # Encabezados
+        encabezados = [
+            "Departamento",
+            "Total Empleados",
+            "Salario Promedio"
+        ]
+
+        for col, texto in enumerate(encabezados):
+            lbl = ctk.CTkLabel(
+                tabla,
+                text=texto,
+                font=ctk.CTkFont(weight="bold")
+            )
+            lbl.grid(row=0, column=col, padx=8, pady=6, sticky="w")
+
+        # Filas
+        for fila, data in enumerate(desc, start=1):
+            valores = [
+                data["departamento"],
+                data["total_empleados"],
+                f"${data['salario_prom']}"
             ]
 
             for col, valor in enumerate(valores):
